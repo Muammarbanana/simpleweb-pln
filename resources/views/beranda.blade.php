cba15885c89636f03080bc0a98c78bec4926fe4b
Muammarbanana <muammarfanani2@gmail.com>
2020-01-13 15:45:41 +0700

    add datatables

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -4,18 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="c-token" content="{!! csrf_token() !!}" />
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    <script src="{{ asset('js/bootstable.js') }}"></script>
     <!-- Styles -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
     <!-- Scripts -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="http://cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
     <style>
         .content {
             font-size:18px;
@@ -29,7 +32,6 @@
         }
         #tomboltambah {
             float: right;
-            margin-bottom: 1%;
             margin-right: 2%;
         }
     </style>
@@ -41,42 +43,46 @@
         <button id="tomboltambah" class="btn btn-success" data-toggle="modal" data-target="#tambahModal">Tambah Proyek</button>
     </div>
     <div class="tabel">
-        <table class="table" id="tabelProyek">
-            <tr>
-                <th>Nomor Surat</th>
-                <th>Pekerjaan</th>
-                <th>Wilayah Kerja</th>
-                <th>Tanggal Awal</th>
-                <th>Tanggal Akhir</th>
-                <th>WBS</th>
-                <th>Keterangan</th>
-                <th>Aksi</th>
-            </tr>
-            @foreach($proyek as $key => $data)
-            <tr>
-                <td>{{$data->Nomor_Surat}}</td>
-                <td>{{$data->Pekerjaan}}</td>
-                <td>{{$data->Wilayah_Kerja}}</td>
-                <td>{{$data->Tanggal_Awal}}</td>
-                <td>{{$data->Tanggal_Akhir}}</td>
-                <td>{{$data->WBS}}</td>
-                <td>{{$data->Keterangan}}</td>
-                <td>
-                    <button class="btn btn-success" data-toggle="modal" data-target="#editModal"
-                        data-id="<?=$data->id_proyek;?>" 
-                        data-no="<?=$data->Nomor_Surat;?>" 
-                        data-pk="<?=$data->Pekerjaan;?>" 
-                        data-wilker="<?=$data->Wilayah_Kerja;?>" 
-                        data-tgl1="<?=$data->Tanggal_Awal;?>" 
-                        data-tgl2="<?=$data->Tanggal_Akhir;?>" 
-                        data-wbs="<?=$data->WBS;?>" 
-                        data-ket="<?=$data->Keterangan;?>">
-                        <i class="fa fa-pencil"></i>
-                    </button>
-                    <button class="btn btn-danger" data-toggle="modal" data-target="#hapusModal" data-no="<?=$data->id_proyek;?>"><i class="fa fa-close"></i></button>
-                </td>
-           </tr>
-           @endforeach
+        <table class="table table-striped" id="tabelProyek">
+            <thead>
+                <tr>
+                    <th>Nomor Surat</th>
+                    <th>Pekerjaan</th>
+                    <th>Wilayah Kerja</th>
+                    <th>Tanggal Awal</th>
+                    <th>Tanggal Akhir</th>
+                    <th>WBS</th>
+                    <th>Keterangan</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($proyek as $key => $data)
+                <tr>
+                    <td>{{$data->Nomor_Surat}}</td>
+                    <td>{{$data->Pekerjaan}}</td>
+                    <td>{{$data->Wilayah_Kerja}}</td>
+                    <td>{{$data->Tanggal_Awal}}</td>
+                    <td>{{$data->Tanggal_Akhir}}</td>
+                    <td>{{$data->WBS}}</td>
+                    <td>{{$data->Keterangan}}</td>
+                    <td>
+                        <button class="btn btn-success" data-toggle="modal" data-target="#editModal"
+                            data-id="<?=$data->id_proyek;?>" 
+                            data-no="<?=$data->Nomor_Surat;?>" 
+                            data-pk="<?=$data->Pekerjaan;?>" 
+                            data-wilker="<?=$data->Wilayah_Kerja;?>" 
+                            data-tgl1="<?=$data->Tanggal_Awal;?>" 
+                            data-tgl2="<?=$data->Tanggal_Akhir;?>" 
+                            data-wbs="<?=$data->WBS;?>" 
+                            data-ket="<?=$data->Keterangan;?>">
+                            <i class="fa fa-pencil"></i>
+                        </button>
+                        <button class="btn btn-danger" data-toggle="modal" data-target="#hapusModal" data-no="<?=$data->id_proyek;?>"><i class="fa fa-close"></i></button>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
         </table>
     </div>
     <!-- Modal Hapus -->
@@ -202,6 +208,25 @@
         </div>
     </div>
     <script>
+        $(document).ready(function(){
+            $('#tabelProyek').DataTable({
+                "language": {
+                    "lengthMenu": "Tampilkan _MENU_ data per halaman",
+                    "zeroRecords": "Data tidak ditemukan",
+                    "info": "Menampilkan _PAGE_ dari _PAGES_",
+                    "infoEmpty": "Data tidak ada",
+                    "infoFiltered": "(disaring dari _MAX_ data total)",
+                    "info": "Menampilkan data _START_ sampai _END_ dari _TOTAL_ data",
+                    "paginate": {
+                        "first":      "Pertama",
+                        "last":       "Terakhir",
+                        "next":       "Selanjutnya",
+                        "previous":   "Sebelumnya"
+                    },
+                    "search": "Cari: "
+        }
+            });
+        });
         $('#hapusModal').on('shown.bs.modal', function (e){
             var data_no = $(e.relatedTarget).data('no');
             var enc_data_no = encodeURIComponent(data_no);
